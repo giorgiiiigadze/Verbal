81886ae7c75edc20bb8248b3ac864cdb519fcacf
Giorgi <giobuisness97@gmail.com>
2026-07-26 11:05:41 +0400

    Initial Commit

---
+++ Verbal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Verbal
+//
+//  Created by giorgi giorgadze on 26/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Verbal/VerbalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbalApp.swift
+//  Verbal
+//
+//  Created by giorgi giorgadze on 26/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct VerbalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
